1d7d0b13b27913a8bcf961d818c2d8014b797aed
Lennard Helbig <lennardhelbig@LaptopvnLennard.fritz.box>
2024-05-09 12:01:59 +0200

    first Commit, Added template Projekt for initial Projekt Structure

---
+++ MovieMaster/MovieMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMasterApp.swift
+//  MovieMaster
+//
+//  Created by Lennard Helbig on 09.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MovieMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
